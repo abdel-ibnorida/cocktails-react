25e47d63770441b2c1e89099814db4f49ee7f2f9
abdelibnorida <abdel.ibnorida@kappas.it>
2023-06-27 19:43:46 +0200

    chore: proj init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Cocktail from './assets/Cocktail.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+
+    </div>
+  )
+}
+
+export default App
