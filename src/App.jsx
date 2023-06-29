e9f1b06cd0deaa5db3537ddbc338155e2c7cf64e
abdelibnorida <abdel.ibnorida@kappas.it>
2023-06-29 15:49:25 +0200

    feat: add single-cocktail-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
 import Cocktail from './assets/Cocktail.svg'
 import './App.css'
+import SingleCocktail from './Components/SingleCocktail';
 
 function App() {
+  const [productSection, setProductSection] = useState("");
+
+  const onRender = () => {
+    switch (productSection) {
+      case "home":
+        return(
+          <div>
+            home
+          </div>
+        )
+      case "":
+        return (
+          <SingleCocktail
+            productSection={productSection}
+            setProductSection={setProductSection}
+          />
+        );
+    }
+  };
 
   return (
-    <div className='App'>
-
+    <div className="App">
+      {onRender()}
     </div>
-  )
+  );
 }
-
 export default App
